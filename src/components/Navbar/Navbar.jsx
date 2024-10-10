60a1a766d241491fbb6bb3e2d14aacc594a8974f
threedev3 <thdev4.1@gmail.com>
2024-10-10 16:06:21 +0500

    Portfolio menu item removed]

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,7 +14,6 @@ const navigation = [
   { name: "About", link: "/about" },
   { name: "Pricing", link: "/pricing" },
   { name: "Case Study", link: "/casestudy" },
-  { name: "Portfolio", link: "/casestudy" },
 ];
 
 function Navbar() {
